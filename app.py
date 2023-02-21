9c9ff11d12aec7256931d7e6d445bd7c6d14aed5
ranamhamoud <94687111+ranamhamoud@users.noreply.github.com>
2023-02-21 10:37:38 +0200

    added 1's and 2's complement

--- app.py
+++ app.py
@@ -5,31 +5,46 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # Render the "home.html" template file
     return render_template('home.html')
+
+# function that converts decimal to binary
 
 
 def decimal_to_binary(number):
     return bin(number)
 
+# function that converts decimal to hexadecimal
+
 
 def decimal_to_hexadecimal(number):
     return hex(number)
+
+# function that converts decimal to octal
 
 
 def decimal_to_octal(number):
     return oct(number)
 
+# function that converts binary to decimal
+
 
 def binary_to_decimal(number):
     return int(number, 2)
+
+# function that converts hexadecimal to decimal
 
 
 def hexadecimal_to_decimal(number):
     return int(number, 16)
 
+# function that converts octal to decimal
+
 
 def octal_to_decimal(number):
     return int(number, 8)
+
+# function that performs the calculation
 
 
 def perform_operation(operator, number1, number2):
@@ -46,8 +61,82 @@ def perform_operation(operator, number1, number2):
     else:
         return "Invalid operator"
 
+# function that gives 1's complement of a binary number
+
+
+def ones_complement(number):
+    # Initialize the ones_complement variable
+    ones_complement = ""
+
+    # Loop through each digit in the binary number
+    for digit in number:
+        # If the digit is 0, append 1 to the ones_complement variable
+        if digit == '0':
+            ones_complement += '1'
+        # If the digit is 1, append 0 to the ones_complement variable
+        elif digit == '1':
+            ones_complement += '0'
+
+    # Return the ones_complement variable
+    return ones_complement
+# function that gives 1's complement of a binary number signed
+
+
+def ones_complement_signed(number):
+    # Initialize the ones_complement variable
+    ones_complement = ""
+
+    # Loop through each digit in the binary number
+    for digit in number:
+        # If the digit is 0, append 1 to the ones_complement variable
+        if digit == '0':
+            ones_complement += '1'
+        # If the digit is 1, append 0 to the ones_complement variable
+        elif digit == '1':
+            ones_complement += '0'
+
+    # Return the ones_complement variable
+    return ones_complement
+
+# function that gives 2's complement of a binary number unsigned
+
+
+def twos_complement(number):
+    # Initialize the twos_complement variable
+    twos_complement = ""
+    # Loop through each digit in the binary number
+    for digit in number:
+        # If the digit is 0, append 1 to the twos_complement variable
+        if digit == '0':
+            twos_complement += '1'
+        # If the digit is 1, append 0 to the twos_complement variable
+        elif digit == '1':
+            twos_complement += '0'
+
+    # Return the twos_complement variable
+    return twos_complement
+
+# function that gives 2's complement of a binary number signed
+
+
+def twos_complement_signed(number):
+    # Initialize the twos_complement variable
+    twos_complement = ""
+    # Loop through each digit in the binary number
+    for digit in number:
+        # If the digit is 0, append 1 to the twos_complement variable
+        if digit == '0':
+            twos_complement += '1'
+        # If the digit is 1, append 0 to the twos_complement variable
+        elif digit == '1':
+            twos_complement += '0'
+
+    # Return the twos_complement variable
+    return twos_complement
+
 
 @app.route('/calculate', methods=['GET', 'POST'])
+# function that calculates the result
 def calculate():
     if request.method == 'POST':
         number1 = request.form['number1']
